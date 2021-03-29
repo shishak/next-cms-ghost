fef4e09b939b68f5ce19dc97f01f0fd0909aa355
Shishak <semenshishak@gmail.com>
2021-03-30 00:45:04 +0300

    Update processEnv.ts

--- lib/processEnv.ts
+++ lib/processEnv.ts
@@ -2,10 +2,10 @@ import * as appConfig from '@appConfig'
 import { NavItem } from '@lib/ghost'
 
 // siteUrl, platform, ghostAPIUrl, ghostAPIKey must be defined here
-export const ghostAPIUrl = process.env.CMS_GHOST_API_URL || 'https://cms.gotsby.org'
-export const ghostAPIKey = process.env.CMS_GHOST_API_KEY || '387f956eaa95345f7bb484d0b8'
+export const ghostAPIUrl = process.env.CMS_GHOST_API_URL || 'http://18.193.174.227'
+export const ghostAPIKey = process.env.CMS_GHOST_API_KEY || '16f3058a677627dcd3611c2698'
 
-const siteUrl = process.env.SITE_URL || (process.env.VERCEL_URL && `https://${process.env.VERCEL_URL}`) || process.env.URL || 'http://localhost:3000'
+const siteUrl = process.env.SITE_URL || (process.env.VERCEL_URL && `https://${process.env.VERCEL_URL}`) || process.env.URL || 'https://nextghosts.netlify.app'
 
 const platform = (process.env.NETLIFY === 'true' && 'netlify') || 'vercel'
 
